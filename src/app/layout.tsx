19b4df2d24efc76952cfb51711b2b9049042824f
MRizainF <icon.firdaus@gmail.com>
2025-05-20 01:09:04 +0700

    style: padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto flex justify-between">
-          <div className="px-2 xsm:px-4 xxl:px-22">
+          <div className="px-2 xsm:px-4 xxl:px-8">
             <LeftBar/>
           </div>
           <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-border-gray">{children}</div>
