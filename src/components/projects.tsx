64bb1a814c7b57ce7ab5172b4b2631b9883db44d
MRizainF <icon.firdaus@gmail.com>
2025-05-21 23:54:54 +0700

    fix: fix img

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
     content: "🧠 Readibly is a dyslexia-friendly search tool that makes reading and navigating text easier with text-to-speech, speech-to-text, and eye-tracking support. Built for real accessibility—fast, simple, and actually useful. 🔍🗣️👁️",
     timestamp: "19 May",
     avatarUrl: "/general/avatar.png",
-    imageUrl: "/post/readibly.png",
+    imageUrl: "/post/readibly-page.png",
     likes: 1,
     comments: 0,
     reposts: 0,
